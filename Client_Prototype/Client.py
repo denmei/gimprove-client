4cd7cb0dba9990111efec087d67cfb9587acff57
dennis <meisnerdennis@web.de>
2018-02-11 16:53:07 +0100

    Refactoring and Multithreading.

--- Client_Prototype/Client.py
+++ Client_Prototype/Client.py
@@ -11,54 +11,16 @@ class Equipment:
     """
 
     def __init__(self, exercise_name, equipment_id):
-        self.list_address = "https://app-smartgym.herokuapp.com/tracker/set_list_rest/"
-        # list_address = "http://127.0.0.1:8000/tracker/set_list_rest/"
-        self.detail_address = "https://app-smartgym.herokuapp.com/tracker/set_detail_rest/"
-        # detail_address = "http://127.0.0.1:8000/tracker/set_detail_rest/"
+        # self.list_address = "https://app-smartgym.herokuapp.com/tracker/set_list_rest/"
+        self.list_address = "http://127.0.0.1:8000/tracker/set_list_rest/"
+        # self.detail_address = "https://app-smartgym.herokuapp.com/tracker/set_detail_rest/"
+        self.detail_address = "http://127.0.0.1:8000/tracker/set_detail_rest/"
 
         self.exercise_name = exercise_name
         self.equipment_id = equipment_id
         self.request_manager = RequestManager(detail_address=self.detail_address, list_address=self.list_address,
                                               exercise_name=self.exercise_name, equipment_id=self.equipment_id)
         self.repetition_manager = RepetitionManager(self)
-
-    """def run(self):
-        
-        Runs the client.
-        
-        self.cache.empty_cache()
-        while self.is_ready():
-            # wait for valid rfid-tag input
-            rfid_tag = input('RFID: ')
-
-            # start recording if rfid-tag is valid
-            if len(rfid_tag) == 10:
-                rep_count = 0
-                ex_weight = 0
-                # create new set
-                response = json.loads(self.new_set(rfid=rfid_tag).decode("utf-8"))
-                set_id = response['id']
-
-                # receive repetitions
-                while True:
-                    rep = input("r for repetition:  ")
-                    if rep == "r":
-                        rep_count += 1
-                        # update set and weight for the first repetition
-                        if rep_count == 1:
-                            ex_weight = random.randint(5, 100)
-                            self.update_set(repetitions=rep_count, weight=ex_weight, set_id=set_id,  rfid=rfid_tag, active=True)
-                        # update set keep weight for the next repetitions after the first one
-                        else:
-                            self.update_set(repetitions=rep_count, weight=ex_weight, set_id=set_id, rfid=rfid_tag, active=True)
-                    else:
-                        # deactivate current set
-                        self.update_set(repetitions=rep_count, weight=ex_weight, set_id=set_id, rfid=rfid_tag, active=False)
-                        break
-            else:
-                print("Not a valid rfid-tag.")
-            print()
-    """
 
     def _init_set_record_(self, rfid):
         """
--- Client_Prototype/RepetitionManager.py
+++ Client_Prototype/RepetitionManager.py
@@ -1,21 +1,44 @@
 import time
 from Client_Prototype.WeightManager import WeightManager
+from threading import Thread
+import os
 
+
+class SendThread(Thread):
+
+    def __init__(self, client, repetition, rfid, set_id, weight_manager):
+        self.client = client
+        self.repetition = repetition
+        self.rfid_tag = rfid
+        self.set_id = set_id
+        self.weight_manager = weight_manager
+        Thread.__init__(self)
+
+    def run(self):
+        # get weight
+        current_weight = self.weight_manager.get_weight_fake()
+        # send update
+        self.client.add_repetition(rfid_tag=self.rfid_tag, set_id=self.set_id, repetitions=self.repetition, weight=current_weight)
 
 
 class RepetitionManager:
 
     def __init__(self, client, timeout_delta=5):
         self.client = client
-        self.weight_manager = WeightManager
+        self.weight_manager = WeightManager()
         self.timeout_delta = timeout_delta
 
     def start(self, set_id, rfid_tag):
         # TODO: weightmanager calibration
+        os.chdir("/home/dennis/PycharmProjects/SmartGym_Client_Prototype/")
         rep = 0
         weight = 0
-        while rep < 5:
+        with open('numbers.txt') as numbers:
+            content = numbers.readlines()
+        for c in content:
+            print(c)
             rep += 1
-            time.sleep(1)
-            self.client.add_repetition(rfid_tag=rfid_tag, set_id=set_id, repetitions=rep, weight=weight)
+            thread = SendThread(client=self.client, repetition=rep, rfid=rfid_tag, set_id=set_id, weight_manager=self.weight_manager)
+            thread.start()
+            time.sleep(0.5)
         self.client.end_set(rfid_tag=rfid_tag, set_id=set_id, repetitions=rep, weight=weight)
