e653744872caaf0bf7b54a61cb56b1528ec28494
dennis <meisnerdennis@web.de>
2018-05-17 20:22:53 +0200

    Install.py

--- install.py
+++ install.py
@@ -1,5 +1,13 @@
 import os
 
 os.system("pip install -r requirements.txt --no-index --find-links file: requirements.txt")
+os.system("sudo apt-get install numpy")
+os.system("sudo apt-get install twisted")
+os.system("sudo apt-get install matplotlib")
+os.system("sudo apt-get install pandas")
+os.system("sudo apt-get install pytz")
+os.system("sudo apt-get install requests")
+os.system("sudo apt-get install autobahn")
+os.system("sudo apt-get install websocket-client")
 os.system("sudo apt-get install python-smbus")
 os.system("sudo apt-get install i2c-tools")
