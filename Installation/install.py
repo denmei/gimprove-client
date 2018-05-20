e88e814e7da45a716d43aaec642aead602157a39
dennis <meisnerdennis@web.de>
2018-05-20 10:56:44 +0200

    install.py2

--- Installation/install.py
+++ Installation/install.py
@@ -1,15 +1,16 @@
 import os
 
 # load required packages and install
-os.system("pip3 install numpy==1.14.2")
-os.system("pip3 install twisted==18.4.0")
+os.system("sudo pip3 install numpy==1.14.2")
+os.system("sudo pip3 install twisted==18.4.0")
 os.system("sudo apt-get install libatlas3-base")
-os.system("pip3 install matplotlib==2.1.1")
-os.system("pip3 install pandas==0.22.0")
-os.system("pip3 install pytz==2018.3")
-os.system("pip3 install requests==2.18.4")
-os.system("pip3 install websocket-client==0.47.0")
-os.system("pip3 install boto3==1.7.23")
+os.system("sudo pip3 install matplotlib==2.1.1")
+os.system("sudo pip3 install pandas==0.22.0")
+os.system("sudo pip3 install pytz==2018.3")
+os.system("sudo pip3 install requests==2.18.4")
+os.system("sudo pip3 install cairocffi")
+os.system("sudo pip3 install websocket-client==0.47.0")
+os.system("sudo pip3 install boto3==1.7.23")
 os.system("sudo apt-get install python-smbus")
 os.system("sudo apt-get install i2c-tools")
 os.system("sudo reboot now")
@@ -20,3 +21,4 @@ os.system("sudo reboot now")
 # os.system("complete -C aws_completer aws")
 
 #TODO: If installation successfull, add file. If file not there, every time execeute.py is used the installation will be done first
+# TODO: ABFRAGE FÜR EQUIPMENT ID
