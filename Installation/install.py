d86c0d54eee64e63dc4f2232df3264cfdfa2a2da
Dennis <meisnerdennis@web.de>
2018-06-24 20:25:47 +0200

    Updated install.py

--- Installation/install.py
+++ Installation/install.py
@@ -4,7 +4,7 @@ from shutil import copy2
 # load required packages and install
 os.system("sudo pip3 install numpy")
 os.system("sudo pip3 install twisted")
-os.system("sudo apt-get install libatlas3-base")
+os.system("sudo apt-get -y install libatlas3-base")
 os.system("sudo pip3 install matplotlib")
 os.system("sudo pip3 install tkinter")
 os.system("sudo pip3 install pandas")
@@ -13,9 +13,12 @@ os.system("sudo pip3 install requests")
 os.system("sudo pip3 install cairocffi")
 os.system("sudo pip3 install websocket-client")
 os.system("sudo pip3 install boto3")
-os.system("sudo apt-get install python-smbus")
-os.system("sudo apt-get install i2c-tools")
-os.system("sudo reboot now")
+os.system("sudo apt-get -y install python-smbus")
+os.system("sudo apt-get -y install i2c-tools")
+os.system("sudo apt-get -y install python-dev python3-dev")
+os.system("sudo apt-get -y install python-rpi.gpio python3-rpi.gpio")
+os.system("git config --global user.email 'none'")
+os.system("git config --global user.name 'pi'")
 
 # maybe necessary for tkinter-problems: sudo apt-get install python3-tk
 
@@ -27,6 +30,8 @@ aws_path = os.path.join(os.path.expanduser("~"), ".aws")
 os.mkdir(aws_path)
 copy2("credentials", os.path.join(aws_path, "credentials"))
 copy2("config", os.path.join(aws_path, "config"))
+
+os.system("sudo reboot now")
 
 #TODO: If installation successfull, add file. If file not there, every time execeute.py is used the installation will be done first
 # TODO: ABFRAGE FÜR EQUIPMENT ID
