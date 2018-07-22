d470ed2b83ef541661e4bfee8b279086dc00d38f
Dennis <meisnerdennis@web.de>
2018-07-22 15:47:02 +0200

    Bugfix for batterytest.

--- client_repo/Client_Prototype/Helpers/battery_test.py
+++ client_repo/Client_Prototype/Helpers/battery_test.py
@@ -9,11 +9,11 @@ class BatteryTest(threading.Thread):
 
     def __init__(self):
         super(BatteryTest, self).__init__()
-        print(os.path.join(str(Path.home()), "battery_test.csv"))
         self.daemon = True
-        self.test_csv = open(os.path.join(str(Path.home()), "battery_test.csv"), "w+")
 
     def run(self):
         while True:
-            self.test_csv.write(str(datetime.datetime.now()))
+            test_csv = open(os.path.join(str(Path.home()), "battery_test.csv"), "w+")
+            test_csv.write(str(datetime.datetime.now()))
+            test_csv.close()
             time.sleep(60)
