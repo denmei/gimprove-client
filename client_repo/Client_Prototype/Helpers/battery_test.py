64285a3d371dedfebb8919d5b0afd4ac43531fb0
Dennis <meisnerdennis@web.de>
2018-07-23 22:11:16 +0200

    Solved battery_test-bug.

--- client_repo/Client_Prototype/Helpers/battery_test.py
+++ client_repo/Client_Prototype/Helpers/battery_test.py
@@ -3,17 +3,19 @@ import time
 import threading
 from pathlib import Path
 import os
+import datetime
 
 
 class BatteryTest(threading.Thread):
 
     def __init__(self):
         super(BatteryTest, self).__init__()
+        self.file_name = "battery_test_" + str(datetime.datetime.now()).replace(" ", "_") + ".csv"
         self.daemon = True
 
     def run(self):
         while True:
-            test_csv = open(os.path.join(str(Path.home()), "battery_test.csv"), "w+")
+            test_csv = open(os.path.join(str(Path.home()), self.file_name), "w+")
             test_csv.write(str(datetime.datetime.now()))
             test_csv.close()
             time.sleep(60)
